3f92d8dc7699c7b1bff1e7c558510f4322cb2f7e
Ricards Lazda <ricards.lazda@magebit.com>
2024-01-03 13:34:22 +0200

    Create Page List Widget

---
+++ app/code/Magebit/PageListWidget/Block/Widget/PageList.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Magebit\PageListWidget\Block\Widget;
+
+use Magento\Cms\Api\Data\PageInterface;
+use Magento\Cms\Api\PageRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class PageList extends Template implements BlockInterface
+{
+    protected $_template = "widget/page_list.phtml";
+
+    protected PageRepositoryInterface $pageRepository;
+    protected SearchCriteriaBuilder $searchCriteriaBuilder;
+
+    public function __construct(Template\Context $context, PageRepositoryInterface $pageRepository, SearchCriteriaBuilder $searchCriteriaBuilder, array $data = [])
+    {
+        parent::__construct($context, $data);
+
+        $this->pageRepository = $pageRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+    }
+
+    /**
+     * @throws LocalizedException
+     */
+    public function getPages(): array
+    {
+        $displayMode = $this->getData('display_mode');
+        $selectedPages = $this->getData('selected_pages');
+
+        if ($displayMode == 'specific_pages' && !empty($selectedPages) && is_array($selectedPages)) {
+            $searchCriteria = $this->searchCriteriaBuilder
+                ->addFilter(PageInterface::TITLE, $selectedPages, 'in')
+                ->create();
+
+            return $this->pageRepository->getList($searchCriteria)->getItems();
+        }
+
+        return $this->pageRepository->getList($this->searchCriteriaBuilder->create())->getItems();
+    }
+}
